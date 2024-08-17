1cc835951e5ff2c1e7668e98664aa5067b8cd7de
nazm0l <tanjirahmednazmul@gmail.com>
2024-08-17 21:58:50 +0600

    Steel City Project Setup

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   speed: 1000,
+//   autoplay: {
+//     delay: 2500,
+//   },
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
