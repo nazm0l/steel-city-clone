55d8f915fad4f5b337b56ef349af0faf90f76e3b
nazm0l <tanjirahmednazmul@gmail.com>
2024-08-27 03:42:08 +0600

    recent work section completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
-//   speed: 1000,
-//   autoplay: {
-//     delay: 2500,
-//   },
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-// });
+$(".slider-items").slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+});
+
+$(".work-slider-prev").on("click", function () {
+  $(".slider-items").slick("slickPrev");
+});
+
+$(".work-slider-next").on("click", function () {
+  $(".slider-items").slick("slickNext");
+});
